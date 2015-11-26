693286595b7e8c2c9b851121ff37ae8110d94851
Liam Sharp <liam.sharp@gmail.com>
2015-11-26 11:56:50 +0000

    Initial commit

---
+++ src/main/java/com/stackoverflow/DemoApp.java
@@ -0,0 +1,36 @@
+package com.stackoverflow;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class DemoApp extends Application
+{
+    
+    @Override
+    public void start(
+        final Stage stage)
+    {
+        stage.setTitle("Demo App");
+        Scene scene = new Scene(new VBox(), 400, 350);
+        scene.setFill(Color.OLDLACE);
+        
+        MenuBar menuBar = new MenuBar();
+        Menu menuFile = new Menu("File");
+        menuBar.getMenus().addAll(menuFile);
+        menuBar.setUseSystemMenuBar(true);
+        
+        ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main( String[] args )
+    {
+        launch(args);
+    }
+}
